d99f471a0c744e44e6b97195a4e60f650876e637
gunjan-yadav40 <yadavgunjan2027@gmail.com>
2026-06-07 16:44:47 +0530

    rate limiting implemented

--- backend/src1/controllers/auth.controller.js
+++ backend/src1/controllers/auth.controller.js
@@ -163,4 +163,4 @@ export const updateProfile = async (req, res) => {
       message: "Internal server error",
     });
   }
-};
+}; 
--- backend/src1/routes/auth.routes.js
+++ backend/src1/routes/auth.routes.js
@@ -7,8 +7,12 @@ import {
 } 
 from "../controllers/auth.controller.js";
 import {protectRoute} from "../middleware/auth.middleware.js"
+import {arcjetProtection} from  "../middleware/arcjet.middleware.js";
 const router = express.Router();
-
+router.get("/test",arcjetProtection,(req,res) => {
+  res.status(200).json({message:"Test Route"});
+});
+router.use(arcjetProtection);
 router.post("/signup" ,signup);
 
 
