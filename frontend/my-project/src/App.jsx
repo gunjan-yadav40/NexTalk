a49b8823d220d570dbe7e8690ee5ff6e01201cb7
gunjan-yadav40 <yadavgunjan2027@gmail.com>
2026-06-09 11:57:45 +0530

    frontend codebase setup

--- frontend/my-project/src/App.jsx
+++ frontend/my-project/src/App.jsx
@@ -1,7 +1,31 @@
+import { Route, Routes } from "react-router";
+import ChatPage from "./pages/ChatPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import { useAuthStore } from "./store/useAuthStore";
+
 function App() {
+  const { authUser, login, isLoggedIn } = useAuthStore();
+  console.log("auth user:", authUser);
+  console.log("isLoggedIn:", isLoggedIn);
   return (
-    <div className="h-screen bg-black text-white flex items-center justify-center text-4xl">
-      Tailwind Working 🚀
+    <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[length:14px_24px]" />
+
+      <div className="absolute top-0 left-4 size-72 bg-pink-500 opacity-20 blur-[100px]" />
+
+      <div className="absolute bottom-0 right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
+
+      <button className="btn btn-primary relative z-20" onClick={login}>
+        Login
+      </button>
+      <div className="relative z-10">
+        <Routes>
+          <Route path="/" element={<ChatPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
